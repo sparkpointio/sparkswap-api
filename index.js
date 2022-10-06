7f77488d0a604178f0e3fe5fa232cba3c1f9a7b4
Aldrick Bonaobra <git@aldrickb.com>
2022-10-06 18:49:04 +0800

    Add initial routes

--- index.js
+++ index.js
@@ -21,6 +21,40 @@ app.get('/', function(req, res) {
   res.send('SparkSwap API!');
 })
 
+app.get('/', function(req, res) {
+  res.send('SparkSwap API!');
+})
+
+app.get('/api/airdrop/:merkle', async function(req, res) {
+  try{
+    const filePath = path.join(process.cwd(), 'src', 'merkle-root-airdrop' , `${req.params.merkle}.json`);
+    const file = fs.readFileSync(filePath, 'utf8');
+    const merkleData = JSON.parse(file);
+
+    res.json({"merkleRoot": merkleData.merkleRoot, "tokenTotal": merkleData.tokenTotal});
+  }
+  catch (e) {
+    if (e.code === 'ENOENT') res.send("Merkle not found");
+    else res.send(e);
+  }
+})
+
+app.get('/api/airdrop/:merkle/claims/:address', async function(req, res) {
+  try{
+    const filePath = path.join(process.cwd(), 'src', 'merkle-root-airdrop' , `${req.params.merkle}.json`);
+    const file = fs.readFileSync(filePath, 'utf8');
+    const merkleData = JSON.parse(file);
+
+    if(typeof merkleData.claims[req.params.address] === 'undefined') throw ('Address not found');
+
+    res.json(merkleData.claims[req.params.address]);
+  }
+  catch (e) {
+    if (e.code === 'ENOENT') res.send("Merkle not found");
+    else res.send(e);
+  }
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
